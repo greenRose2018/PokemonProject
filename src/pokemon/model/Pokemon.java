e4e5b6a9adf42baa0cdd0ac361503c7e8b562b02
greenRose2018 <brifun13@gmail.com>
2017-12-14 09:10:06 -0700

    added code to methods in Pokemon class
    added code to methods: Pokemon(), getPokemonTypes(), and toString().

--- src/pokemon/model/Pokemon.java
+++ src/pokemon/model/Pokemon.java
@@ -11,16 +11,36 @@ public class Pokemon
 	
 	public Pokemon(String name, int number)
 	{
+		this.name = name;
+		this.number = number;
 	}
 	
 	public final String[] getPokemonTypes()
 	{
+		Class<?> [] types = getClass().getInterfaces();
+		String [] pokeTypes = new String[types.length];
+		
+		for(int index = 0; index < types.length; index++)
+		{
+			String currentInterface = types[index].getCanonicalName();
+			currentInterface = currentInterface.replace(this.getClass().getPackage().getName() + ".", "");
+			pokeTypes[index] = currentInterface;
 		}
 		
+		return pokeTypes;
 	}
 	
 	public String toString()
 	{
+		String response = "Hi, I am a " + name + ", and my HP is " + healthPoints;
+		return response;
+	}
+	public String getPokemonInformation()
+	{
+		String info = "his pokemon is of type: " + this.getClass().getSimpleName();
+		return info;
+	}
+
 	}
 	
 }
