e504bf007c72ca03c8f7029a0985e7cfa181d55b
greenRose2018 <brifun13@gmail.com>
2017-12-14 08:53:26 -0700

    created model and added datamembers and methods, followed umlet design

---
+++ src/pokemon/model/Pokemon.java
@@ -0,0 +1,26 @@
+package pokemon.model;
+
+public class Pokemon
+{
+	private int healthPoints;
+	private int attackPoints;
+	private double enhancementModifier;
+	private int number;
+	private String name;
+	private boolean canEvolve;
+	
+	public Pokemon(String name, int number)
+	{
+	}
+	
+	public final String[] getPokemonTypes()
+	{
+		}
+		
+	}
+	
+	public String toString()
+	{
+	}
+	
+}
