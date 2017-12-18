078566b79602895fb9ee99bc770e756bba5f6f3e
greenRose2018 <brifun13@gmail.com>
2017-12-18 09:19:33 -0700

    Horsea extends pokemon implements water, added methods

--- src/pokemon/model/pokemon/Horsea.java
+++ src/pokemon/model/pokemon/Horsea.java
@@ -1,6 +1,26 @@
 package pokemon.model.pokemon;
 
-public class Horsea
-{
+import pokemon.model.Pokemon;
+import pokemon.model.types.Water;
 
+public class Horsea extends Pokemon implements Water
+{
+	public Horsea(int number, String name)
+	{
+		super(number,name);
+	}
+	public String sayName()
+	{
+		return "Horsea";
+	}
+	
+	public boolean canSwim()
+	{
+		return true;
+	}
+	
+	public int waterGun()
+	{
+		return 15;
+	}
 }
