bb3a023efbd1b0eb6eb7883713577bd59ddc9189
greenRose2018 <brifun13@gmail.com>
2017-12-18 09:20:34 -0700

    Kingdra extends Horsea implements Dragon, added constructor and methods

--- src/pokemon/model/pokemon/Kingdra.java
+++ src/pokemon/model/pokemon/Kingdra.java
@@ -1,6 +1,24 @@
 package pokemon.model.pokemon;
 
-public class Kingdra
-{
+import pokemon.model.types.Dragon;
 
+public class Kingdra extends Horsea implements Dragon
+{
+	public Kingdra(int number, String name)
+	{
+		super(number, name);
+	}
+	public String sayName()
+	{
+		return "Kingdra";
+	}
+	
+	public boolean canFly()
+	{
+		return false;
+	}
+	public int dragonTail()
+	{
+		return 15;
+	}
 }
