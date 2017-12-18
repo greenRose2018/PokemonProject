ecbad5a4bd993ba34c5437959d4cdf0bb7f02e2a
greenRose2018 <brifun13@gmail.com>
2017-12-18 08:57:09 -0700

    Cubchoo class implements ice, added methods

--- src/pokemon/model/pokemon/Cubchoo.java
+++ src/pokemon/model/pokemon/Cubchoo.java
@@ -1,6 +1,27 @@
 package pokemon.model.pokemon;
 
-public class Cubchoo
-{
+import pokemon.model.Pokemon;
+import pokemon.model.types.Ice;
 
+public class Cubchoo extends Pokemon implements Ice
+{
+	public Cubchoo(int number, String name)
+	{
+		super(number,name);
+	}
+	
+	public String sayName()
+	{
+		return "Cubchoo";
+	}
+	
+	public int powderSnow()
+	{
+		return 15;
+	}
+	
+	public int icyWind()
+	{
+		return 10;
+	}
 }
