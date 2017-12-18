5138344689dd3ca0d1a3f209dc88a942b0874127
greenRose2018 <brifun13@gmail.com>
2017-12-18 09:20:01 -0700

    Seadra extends Horsea added constructor

--- src/pokemon/model/pokemon/Seadra.java
+++ src/pokemon/model/pokemon/Seadra.java
@@ -1,6 +1,13 @@
 package pokemon.model.pokemon;
 
-public class Seadra
+public class Seadra extends Horsea
 {
-
+	public Seadra(int number, String name)
+	{
+		super(number,name);
+	}
+	public String sayName()
+	{
+		return "Seadra";
+	}
 }
