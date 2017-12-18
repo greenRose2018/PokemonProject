02ccc5191f790fd1052f0d2378cd8c9a962ac4cf
greenRose2018 <brifun13@gmail.com>
2017-12-18 08:36:48 -0700

    Litleo class implemented normal and fire, added methods

--- src/pokemon/model/pokemon/Litleo.java
+++ src/pokemon/model/pokemon/Litleo.java
@@ -1,6 +1,35 @@
 package pokemon.model.pokemon;
 
-public class Litleo
+import pokemon.model.Pokemon;
+import pokemon.model.types.Normal;
+import pokemon.model.types.Fire;
+
+public class Litleo extends Pokemon implements Normal, Fire
 {
+	public Litleo(int number, String name)
+	{
+		super(number,name);
+	}
+	public boolean hasEffectTailWhip()
+	{
+		return true;
+	}
+	public int takeDown()
+	{
+		return 15;
+	}
+	public String sayName()
+	{
+		return "Litleo";
+	}
+	
+	public int flame()
+	{
+		return 5;
+	}
+	public int ember()
+	{
+		return 3;
+	}
 
 }
