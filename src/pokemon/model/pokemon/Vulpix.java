b5a242a0816d1958bb77d00a93c05e8343e09450
greenRose2018 <brifun13@gmail.com>
2017-12-18 08:53:10 -0700

    Vulpix class implements Fire, added methods

--- src/pokemon/model/pokemon/Vulpix.java
+++ src/pokemon/model/pokemon/Vulpix.java
@@ -1,6 +1,27 @@
 package pokemon.model.pokemon;
 
-public class Vulpix
-{
+import pokemon.model.Pokemon;
+import pokemon.model.types.Fire;
 
+public class Vulpix extends Pokemon implements Fire
+{	
+	public Vulpix(int number, String name)
+	{
+		super(number,name);
+	}
+	
+	public int flame()
+	{
+		return 5;
+	}
+	
+	public int ember()
+	{
+		return 5;
+	}
+	
+	public String sayName()
+	{
+		return "Vulpix";
+	}
 }
