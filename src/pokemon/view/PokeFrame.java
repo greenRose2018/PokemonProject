cb256918412c17f8aaa5e33f5d6ebace08470613
greenRose2018 <brifun13@gmail.com>
2017-12-18 09:58:35 -0700

    found bug need to fix bug

--- src/pokemon/view/PokeFrame.java
+++ src/pokemon/view/PokeFrame.java
@@ -4,7 +4,34 @@ import pokemon.controller.Controller;
 import javax.swing.JFrame;
 
 
-public class PokeFrame
-{
+public class PokeFrame extends JFrame
+{	
+	private Controller baseController;
+	private PokePanel firstPanel;
 
+	public PokeFrame(Controller baseController)
+	{
+		super();
+		this.setBaseController(baseController);
+		firstPanel = new PokePanel(baseController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(firstPanel);
+		this.setTitle("Pokedex");
+		this.setSize(800, 800);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+	
+	public Controller getBaseController()
+	{
+		return baseController;
+	}
+	public void setBaseController(Controller baseController)
+	{
+		this.baseController = baseController;
+	}
 }
