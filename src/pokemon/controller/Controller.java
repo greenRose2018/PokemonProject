568e5efa4782ea9f95f43087a20bb742981cb610
greenRose2018 <brifun13@gmail.com>
2018-01-02 09:58:15 -0700

    created list and methods isValidInteger, and isValidDouble

--- src/pokemon/controller/Controller.java
+++ src/pokemon/controller/Controller.java
@@ -1,10 +1,29 @@
 package pokemon.controller;
 
+import pokemon.model.Pokemon;
 import pokemon.view.PokeFrame;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Controller
 {
 	//MARK: Data-members
+	private List<Pokemon> pokedex;
+	
+	public List<Pokemon> getPokedex()
+	{
+		return pokedex;
+	}
+	
+	public boolean isValidInteger(String input)
+	{
+		return false;
+	}
+	public boolean isValidDouble(String input)
+	{
+		return false;
+	}
+	
 	private PokeFrame appFrame;
 	
 	public Controller()
