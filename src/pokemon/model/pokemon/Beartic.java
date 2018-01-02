eb5af336d2acc272464823f1dad143e8d76fb839
greenRose2018 <brifun13@gmail.com>
2018-01-02 09:04:43 -0700

    Cubchoo, Beartic, in all added/setter for atk points,canEvolve, Enhance, and healthPoints for each file

--- src/pokemon/model/pokemon/Beartic.java
+++ src/pokemon/model/pokemon/Beartic.java
@@ -2,8 +2,24 @@ package pokemon.model.pokemon;
 
 public class Beartic extends Cubchoo
 {
+	public Beartic()
+	{
+		super(614,"Beartic");
+	}
+	public Beartic(String name)
+	{
+		super(614,name);
+	}
 	public Beartic(int number, String name)
 	{
 		super(number, name);
+		setup();
+	}
+	@Override
+	protected void setup()
+	{
+		this.setAttackPoints(49);
+		this.setCanEvolve(false);
+		this.setHealthPoints(210);
 	}
 }
--- src/pokemon/model/pokemon/Cubchoo.java
+++ src/pokemon/model/pokemon/Cubchoo.java
@@ -5,9 +5,18 @@ import pokemon.model.types.Ice;
 
 public class Cubchoo extends Pokemon implements Ice
 {
+	public Cubchoo()
+	{
+		super(613,"Cubchoo");
+	}
+	public Cubchoo(String name)
+	{
+		super(613,name);
+	}
 	public Cubchoo(int number, String name)
 	{
 		super(number,name);
+		setup();
 	}
 	
 	public String sayName()
@@ -23,5 +32,12 @@ public class Cubchoo extends Pokemon implements Ice
 	public int icyWind()
 	{
 		return 10;
+	}
+	protected void setup()
+	{
+		this.setAttackPoints(15);
+		this.setCanEvolve(true);
+		this.setEnhancementModifier(.90);
+		this.setHealthPoints(43);
 	}
 }
