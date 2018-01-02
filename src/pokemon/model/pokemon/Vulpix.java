b16cd29da8d5e8d90ff846b307c7445659458092
greenRose2018 <brifun13@gmail.com>
2018-01-02 08:44:46 -0700

    Vulpix, added/setter for atk points,canEvolve, Enhance, and healthPoints

--- src/pokemon/model/pokemon/Vulpix.java
+++ src/pokemon/model/pokemon/Vulpix.java
@@ -5,9 +5,18 @@ import pokemon.model.types.Fire;
 
 public class Vulpix extends Pokemon implements Fire
 {	
+	public Vulpix()
+	{
+		super(037, "Vulpix");
+	}
+	public Vulpix(String name)
+	{
+		super(037, name);
+	}
 	public Vulpix(int number, String name)
 	{
 		super(number,name);
+		setup();
 	}
 	
 	public int flame()
@@ -23,5 +32,12 @@ public class Vulpix extends Pokemon implements Fire
 	public String sayName()
 	{
 		return "Vulpix";
+	}
+	protected void setup()
+	{
+		this.setAttackPoints(15);
+		this.setCanEvolve(true);
+		this.setEnhancementModifier(.75);
+		this.setHealthPoints(60);
 	}
 }
