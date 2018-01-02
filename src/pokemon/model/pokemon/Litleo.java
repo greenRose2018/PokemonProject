893a3003a73a2895a098e1c97466721246913563
greenRose2018 <brifun13@gmail.com>
2018-01-02 08:54:11 -0700

    Litleo,added/setter for atk points,canEvolve, Enhance, and healthPoints

--- src/pokemon/model/pokemon/Litleo.java
+++ src/pokemon/model/pokemon/Litleo.java
@@ -6,9 +6,18 @@ import pokemon.model.types.Fire;
 
 public class Litleo extends Pokemon implements Normal, Fire
 {
+	public Litleo()
+	{
+		super(667,"Litleo");
+	}
+	public Litleo(String name)
+	{
+		super(667, name);
+	}
 	public Litleo(int number, String name)
 	{
 		super(number,name);
+		setup();
 	}
 	public boolean hasEffectTailWhip()
 	{
@@ -30,6 +39,14 @@ public class Litleo extends Pokemon implements Normal, Fire
 	public int ember()
 	{
 		return 3;
+	}
+	
+	protected void setup()
+	{
+		this.setAttackPoints(16);
+		this.setCanEvolve(true);
+		this.setEnhancementModifier(.70);
+		this.setHealthPoints(58);
 	}
 
 }
