726d21ea8ba883209d17da68ea5ac7110d8f9bbb
greenRose2018 <brifun13@gmail.com>
2018-01-02 09:53:47 -0700

    added componets to JPanel

--- src/pokemon/view/PokePanel.java
+++ src/pokemon/view/PokePanel.java
@@ -4,16 +4,54 @@ import pokemon.controller.Controller;
 import javax.swing.*;
 import java.awt.*;
 
-
 public class PokePanel extends JPanel
 {
-	private Controller baseController;
+	private Controller appController;
+	private SpringLayout baseLayout;
 	
+	private JLabel healthLabel;
+	private JLabel attackLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel evolvableLabel;
+	private JLabel modifierLabel;
+	private JLabel iconLabel;
 	
+	private JCheckBox evolvableBox;
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextField attackField;
+	private JTextField healthField;
+	private JTextField modifierField;
+	
+	private JTextArea descriptionArea;
+	private JTextArea typeArea;
+	
+	private JButton saveButton;
+	private JButton clearButton;
+	private JComboBox pokedexDropdown;
+	
+	private JPanel firstType;
+	private JPanel secondType;
+	private JPanel thirdType;
+	private JPanel fourthType;
+	private JPanel fifthType;
+	private JPanel sixthType;
+	private JPanel seventhType;
+	
+	private void updatePokedexInfo(int index)
+	{
+		nameField.setText(appController.getPokedex().get(index).getName());
+		evolvableBox.setSelected(appController.getPokedex().get(index).isCanEvolve());
+		numberField.setText(appController.getPokedex().get(index).getNumber() + "");
+		attackField.setText(appController.getPokedex().get(index).getAttackPoints() + "");
+		healthField.setText(appController.getPokedex().get(index).getHealthPoints() + "");
+		modifierField.setText(appController.getPokedex().get(index).getEnhancementModifier() + "");
+	}
 	public PokePanel() 
 	{
 		super();
-		this.baseController = baseController;
+		this.appController = appController;
 		
 		setupPanel();
 		setupLayout();
