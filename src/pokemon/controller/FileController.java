b5e90809fbdf2abbe3e53300c5dccc1096e8b66e
greenRose2018 <brifun13@gmail.com>
2018-01-17 09:43:21 -0700

    created new class file and added imports for FileController

---
+++ src/pokemon/controller/FileController.java
@@ -0,0 +1,12 @@
+package pokemon.controller;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import pokemon.model.Pokemon;
+import java.util.ArrayList;
+
+public class FileController
+{
+}
